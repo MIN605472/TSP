d7095806e2ee1c489cd5800fe0c690a5d73c99a9
MIN605472 <MIN605472@users.noreply.github.com>
2017-05-29 21:31:20 +0200

    Added a script to run some benchmarks for the different algorithms

---
+++ run_benchmark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ulimit -t 1200 # 20 minutes
+INPUT_FILES="tspbenchmarks/*.tsp"
+OPTIONS="fb av rp pd"
+for OPTION in $OPTIONS; do
+  for FILE in $INPUT_FILES; do
+    echo -e "\033[0;32m $OPTION $FILE \033[0m"
+    time build/tsp/tsp -${OPTION} $FILE > /dev/null
+  done
+done
