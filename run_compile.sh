0872984c9056ad8a5285b98582ece58d6d2afa26
MIN605472 <MIN605472@users.noreply.github.com>
2017-05-31 00:17:49 +0200

    Improved compile script

--- run_compile.sh
+++ run_compile.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
-# If I don't to this, CMake would identify an old version of GCC in that
-# machine
-if [ ${HOSTNAME} = "lab000" ]; then
-  export CXX=$(which g++)
-fi
-
+export CXX=$(which g++)
 if [ ! -d "build" ]; then
   mkdir build
 fi
