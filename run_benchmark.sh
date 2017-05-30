cc1f26c630e8cce5b3663ee6183d74f686817844
MIN605472 <MIN605472@users.noreply.github.com>
2017-05-30 23:27:19 +0200

    Modified the benchmark script now that it doesn't need to measure the elapsed time

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ulimit -t 1200 # 20 minutes
-INPUT_FILES="benchmark_input/random/*.tsp"
+ulimit -t 1800 # 30 minutes
+INPUT_FILES=$(ls -v1 benchmark_input/random/*.tsp)
 OPTIONS="fb av rp pd"
-for OPTION in $OPTIONS; do
-  for FILE in $INPUT_FILES; do
-    echo -e "\033[0;32m $OPTION $FILE \033[0m"
-    time build/tsp/tsp -${OPTION} $FILE > /dev/null
+for OPTION in ${OPTIONS}; do
+  for FILE in ${INPUT_FILES}; do
+    echo -e "\033[0;32m${OPTION} $(echo ${FILE} | cut -f3 -d'/')\033[0m"
+    build/tsp/tsp -${OPTION} ${FILE} 2>&1 | tail -n 2
   done
 done
