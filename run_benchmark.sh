de5a8efe9021fb149979bb21b050a656f7f56a50
MIN605472 <MIN605472@users.noreply.github.com>
2017-05-31 20:54:20 +0200

    Improved benchmark script, the output has CSV like formating

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-ulimit -t 1800 # 30 minutes
 INPUT_FILES=$(ls -v1 benchmark_input/random/*.tsp)
 OPTIONS="fb av rp pd"
+NUM_SAMPLES="5"
+TO="false"
 for OPTION in ${OPTIONS}; do
-  for FILE in ${INPUT_FILES}; do
-    echo -e "\033[0;32m${OPTION} $(echo ${FILE} | cut -f3 -d'/')\033[0m"
-    build/tsp/tsp -${OPTION} ${FILE} 2>&1 | tail -n 2
+  echo ${OPTION}
+  echo "${INPUT_FILES}" | while read FILE && [ ${TO} = "false" ]; do
+    echo -en "$(echo ${FILE} | cut -f3 -d'/');"
+    SAMPLE=0
+    while [ ${SAMPLE} -lt ${NUM_SAMPLES} -a ${TO} = "false" ]; do
+      RES=$(timeout 2h bash -c "build/tsp/tsp -${OPTION} ${FILE} 2>&1 | tail -n 1")
+      if [ $? -eq  124 ]; then
+        TO="true"
+      fi
+      echo -n "${RES};"
+      SAMPLE=$(expr ${SAMPLE} + 1)
+    done
+    echo
   done
+  TO="false"
 done
--- run_compile.sh
+++ run_compile.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
 export CXX=$(which g++)
+
 if [ ! -d "build" ]; then
   mkdir build
+fi
+
+if [ ! -d "benchmark_input" ]; then
+  mkdir benchmark_input
 fi
 
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
+cd ../benchmark_input
+../build/tsp_gen/tsp_gen 50
